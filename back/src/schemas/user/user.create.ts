1d9bfc22f737650b4f74e54d0c3c8c75167c5555
Ezequiel Trindade <ezequiel.trindade.pb@compass.oul>
2026-05-19 20:43:38 -0300

    removed: back, server e backend

--- back/src/schemas/user/user.create.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod"
-
-export const createUserSchema = z.object({
-  nome: z.string().min(3),
-  email: z.email(),
-  password: z.string().min(6),
-  tipo: z.enum(["ADMIN", "USUARIO"]),
-  id_escola: z.number().optional(),
-})
