a011cb347f7c9d619c012d0d0c223c8443adf57d
Ezequiel Trindade <ezequiel.trindade.pb@compass.oul>
2026-06-28 20:26:35 -0300

    preparação para o turso adicionada

--- back/src/db/prisma-client.ts
+++ back/src/db/prisma-client.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
+
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+// const adapter = new PrismaLibSQL({
+//   url: process.env.DATABASE_URL!,
+//   authToken: process.env.DATABASE_AUTH_TOKEN!,
+// });
+
+// export const prisma = new PrismaClient({
+//   adapter,
+// });
