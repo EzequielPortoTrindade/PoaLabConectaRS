78d8244c1c710406b3df582b6f19fdfe66e114c8
Ezequiel_Porto_Trindade <142227217+EzequielPortoTrindade@users.noreply.github.com>
2026-08-24 23:08:30 +0000

    configura Prisma com SQLite

---
+++ prisma/migrations/20260824230522_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Item_Capital" (
+    "id_itemCapital" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "n_patrimonio" TEXT NOT NULL,
+    "n_serie" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "estado" TEXT NOT NULL,
+    "modelo" TEXT NOT NULL
+);
